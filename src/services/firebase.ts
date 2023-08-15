d251260b239efc43ca6d5c346f60110c697835f9
João <joaocruz375@gmail.com>
2023-08-15 02:49:00 -0300

    Revert "first commit"
    This reverts commit adc1efdb2ef3f43824178409a43b1aec24f3a55e.
    
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch main
    Your branch is up to date with 'origin/main'.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCxBxokPiKbfy57sINI_qkEZI9uWEOgx2M",
-  authDomain: "juliana-marriage-page.firebaseapp.com",
-  projectId: "juliana-marriage-page",
-  storageBucket: "juliana-marriage-page.appspot.com",
-  messagingSenderId: "781253637764",
-  appId: "1:781253637764:web:1f594a59d8537200b7f4d1"
-});
 
-export const db = getFirestore(firebaseApp);
+const firebaseConfig = {
+  apiKey: "AIzaSyASEh7Akql0m-IAxG-oe1rKZPIeI8KTB6s",
+  authDomain: "marry-invite-page.firebaseapp.com",
+  projectId: "marry-invite-page",
+  storageBucket: "marry-invite-page.appspot.com",
+  messagingSenderId: "110536542869",
+  appId: "1:110536542869:web:b3ec3b92806d92689ae99c"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
